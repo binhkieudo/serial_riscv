82241ed53d0758c52c1c5e706b13173e693041ed
binhkieudo <binhkieudo@gmail.com>
2023-08-17 03:56:31 +0900

    update 230817_0356

---
+++ deploy/Arty_A7/Arty_A7.srcs/sources_1/new/top_artya7.v
@@ -0,0 +1,66 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/17/2023 01:39:38 AM
+// Design Name: 
+// Module Name: top_artya7
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_artya7(
+        input  wire i_clk,
+        input  wire i_rstn,
+        // GPIOs
+        output wire q,
+        // JTAG
+        input  wire jtag_trst,
+        input  wire jtag_tdi,
+        input  wire jtag_tms,
+        input  wire jtag_tck,
+        output wire jtag_tdo
+    );
+    
+    reg [3:0] r_rstn = 4'b0000;
+    
+    always @(posedge i_clk)
+        if (!i_rstn) r_rstn <= 4'b0000;
+        else r_rstn <= {r_rstn[2:0], 1'b1};
+    
+    servant # (
+        .memsize    (8192           ),
+        .RESET_ADDR (32'h0000_0000  ),
+        .E_EXT      (1              ),
+        .CSR_COUNT  (8              ) 
+    )
+    u0 (
+     .wb_clk        (i_clk      ),
+     .wb_rstn       (r_rstn[3]  ),
+     // GPIO
+     .q             (q          ),
+     // JTAG
+     .i_jtag_trst   (jtag_trst  ),
+     .i_jtag_tck    (jtag_tck   ),
+     .i_jtag_tdi    (jtag_tdi   ),
+     .o_jtag_tdo    (jtag_tdo   ),
+     .i_jtag_tms    (jtag_tms   ),
+     // Flash control
+     .o_flash_SCK   ( ),
+     .o_flash_CSn   ( ),
+     .o_flash_MOSI  ( ),
+     .i_flash_MISO  (1'b0 )
+    );
+    
+endmodule
